e1a052f846f1d3752d7644e674c0355a18018db7
nklee <lng1982@gmail.com>
2017-08-02 17:56:07 +0900

    google app engine 테스트

--- config-service/src/main/java/com/example/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -9,6 +9,5 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
-	}
+		SpringApplication.run(ConfigServiceApplication.class, args); }
 }
