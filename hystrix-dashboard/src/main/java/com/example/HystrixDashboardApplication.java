a6c236000d7cd7119a8b19bcca52d02898e0e85b
Lee.jeongsoo <dlwjdtn535@naonsoft.com>
2019-06-11 11:29:33 +0900

    #spring-cloud Service Study..

--- hystrix-dashboard/src/main/java/com/example/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/example/HystrixDashboardApplication.java
@@ -1,11 +1,11 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
 
-@EnableDiscoveryClient
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
--- order-service/src/main/java/com/example/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/OrderServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,17 +12,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
+	
+	@RestController
+	static class Controller {
+		@GetMapping("/test")
+		@HystrixCommand(fallbackMethod = "fallback")
+	    String test(@RequestParam String path) {
+			
+			if(path.equals("1")){
+				throw new RuntimeException("ERROR");
+			}else{
+				return "test1232123123123";
+			}
+	    }
+		
+		private String fallback(String path) {
+			return "hello fallback";
+		}
+	}
+	
+	
 }
 
 @RefreshScope
@@ -30,6 +56,6 @@ class TestController {
 
     @RequestMapping("/welcome")
     String welcome() {
-        return "welcome order service!! #1";
+        return "welcome order service!454545! #1";
     }
 }
